345daa748dbba57e23208e78e962a21fbcba2f81
Tim Manser <manserdev@protonmail.com>
2024-03-17 15:52:59 +1100

    added if statement on line 15 to handle the option to exit the program.
    function added on line 29 to clear input for next iteration.

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,20 @@ use std::io;
 fn main() {
     let mut celcius = String::new();
 
-    while celcius != "exit"  {
+    loop  {
         println!("Please enter celsius to calculate Fahrenheit: ");
         println!("Or type 'exit' to end.");
 
-
+        //takes user input
         io::stdin()
             .read_line(&mut celcius)
             .expect("incorrect try again");
 
+        if celcius.trim() == "exit" {
+            break; // exits the loop if the user types 'exit'
+        }
+
+        //parses the string into a float
         let celcius: f32 = celcius
             .trim()
             .parse()
@@ -21,6 +26,7 @@ fn main() {
 
         println!("{celcius} converted to Fahrenheit is: {converted_value}");
 
+        celcius.clear(); //Clear the input for the next iteration of the loop
 
     }
 }
